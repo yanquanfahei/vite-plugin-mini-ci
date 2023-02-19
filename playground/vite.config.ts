06f01d7873691130ebdaf9319c0b52714280793a
yanquanfahei <1449826851@qq.com>
2023-02-19 13:14:14 +0800

    test: preview

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -6,6 +6,7 @@ import miniCI from '../src/index'
 export default defineConfig({
   plugins: [uni(), miniCI({
     alipayOpt: {
+      appId: '2021003139676331',
       appPath: 'D:\\mp-alipay\\小程序开发者工具'
     }
   })]
