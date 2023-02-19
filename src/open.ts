52437c7ad73ca1ca765ccaa33373c088529d8c4c
yanquanfahei <1449826851@qq.com>
2023-02-19 13:14:26 +0800

    types: update

--- src/open.ts
+++ src/open.ts
@@ -1,9 +1,5 @@
 import { minidev } from 'minidev'
 
-export interface AlipayOpt {
-  appPath?: string,
-}
-
 export function openAlipayTool (appPath?: string, projectPath?: string) {
   minidev.startIde({
     appPath,
